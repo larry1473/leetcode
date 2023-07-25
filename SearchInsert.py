1581773d577d24a1ea7c6b740eea0c207069f406
larry147 <fotsoguiffo8@gmail.com>
2023-07-24 23:10:44 -0400

    comments changes

--- SearchInsert.py
+++ SearchInsert.py
@@ -5,8 +5,8 @@
  * This code solves the leetcode  problem insert search . 
  * the trick here is to keep in mind that you are working on a 
  * dynamic array an every time you delete and element the array size changes. 
- * Memory usage:16.33 MB.
- * time taken by the code to solve the problem: 45 ms.
+ * Memory usage:17.14 MB.
+ * time taken by the code to solve the problem: 68 ms.
  * Date: 2023-07-24.
  */
 
