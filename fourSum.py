66eb7d088e58f746d528a88b38084873087e9356
larry fotso <fotsoguiffo8@gmail.com>
2026-02-03 13:40:15 -0500

    fourSum better version

--- fourSum.py
+++ fourSum.py
@@ -1,17 +1,19 @@
-def fourSum(nums,target):
+def fourSum(nums, target):
     nums.sort()
     n = len(nums)
     res = set()
-    for i in range(n):
-        for j in range (i+1, n-2):
+
+    for i in range(n - 3):
+        for j in range(i + 1, n - 2):
             new_target = target - (nums[i] + nums[j])
             seen = {}
-            new_nums = nums[j+1:]
-            for k, num in enumerate(new_nums):
+
+            for k in range(j + 1, n):
+                num = nums[k]
                 if num in seen:
-                    res.add((nums[i],nums[j],new_nums[seen[num]],num))
+                    res.add((nums[i], nums[j], nums[seen[num]], num))
                 else:
                     seen[new_target - num] = k
-             
+
     return [list(t) for t in res]
         
