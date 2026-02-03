c81faf7795639345fbf96c3da0270f23c77485c7
larry fotso <fotsoguiffo8@gmail.com>
2026-02-03 13:17:13 -0500

    foursum solution

--- fourSum.py
+++ fourSum.py
@@ -1,11 +1,17 @@
-def four_sum(nums,target):
+def fourSum(nums,target):
     nums.sort()
     n = len(nums)
+    res = set()
     for i in range(n):
-        for j in range(i+1, n - 3 ):
-            left = i+1 
-            right = n -1 
-            while left < right:
-                # pick the right pair of (left,right)
-                pass
+        for j in range (i+1, n-2):
+            new_target = target - (nums[i] + nums[j])
+            seen = {}
+            new_nums = nums[j+1:]
+            for k, num in enumerate(new_nums):
+                if num in seen:
+                    res.add((nums[i],nums[j],new_nums[seen[num]],num))
+                else:
+                    seen[new_target - num] = k
+             
+    return [list(t) for t in res]
         
